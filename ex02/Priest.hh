06b6665b36aa3ad7c51e73e83bc009296696330e
Watson <you@example.com>
2018-07-06 15:18:01 +0800

    my commit

---
+++ ex02/Priest.hh
@@ -0,0 +1,36 @@
+#ifndef PRIEST_H_
+#define PRIEST_H_
+
+#include <string>
+#include <iostream>
+
+#include "Mage.hh"
+
+class Priest : public Mage {
+protected:
+	Priest();
+
+public:
+	explicit Priest(std::string const & name, int lvl);
+	virtual ~Priest();
+
+	virtual int CloseAttack();
+
+protected:
+	virtual void speakCreated() const;
+	virtual void speakCloseAttack() const;
+	virtual void speakHeal() const;
+
+	static int const baseForce = 4;
+	static int const baseEndurance = 4;
+	static int const baseIntelligence = 42;
+	static int const baseEsprit = 21;
+	static int const baseAgilite = 2;
+
+	static int const baseAtkClose = 10;
+	static int const baseRestorePv = 70;
+	static int const baseCostPv = 10;
+
+};
+
+#endif
